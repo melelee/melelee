7cc17094375629076c85ad28861e4763edd47e3b
melelee <melelee@qq.com>
2019-02-13 17:31:17 +0800

    证书提交

--- src/main/java/com/melelee/melelee/MeleleeApplication.java
+++ src/main/java/com/melelee/melelee/MeleleeApplication.java
@@ -2,19 +2,11 @@ package com.melelee.melelee;
 
 import com.didispace.swagger.EnableSwagger2Doc;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 
 /**
@@ -32,57 +24,57 @@ public class MeleleeApplication {
 		SpringApplication.run(MeleleeApplication.class, args);
 	}
 
-	/**
-	 * 会自动注册使用了@ServerEndpoint注解声明的Websocket endpoint
-	 * 要注意，如果使用独立的servlet容器，
-	 * 而不是直接使用springboot的内置容器，
-	 * 就不要注入ServerEndpointExporter，因为它将由容器自己提供和管理。
-	 */
-	@Bean
-	public ServerEndpointExporter serverEndpointExporter() {
-		return new ServerEndpointExporter();
-	}
-
-
-	@Bean
-	public Connector httpConnector() {
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-
-		//Set the scheme that will be assigned to requests received through this connector
-		//@param scheme The new scheme
-		connector.setScheme("http");
-
-		//Set the port number on which we listen for requests.
-		// @param port The new port number
-		connector.setPort(80);
-
-		//Set the secure connection flag that will be assigned to requests received through this connector.
-		//@param secure The new secure connection flag
-		//if connector.setSecure(true),the http use the http and https use the https;else if connector.setSecure(false),the http redirect to https;
-		connector.setSecure(false);
-
-		//redirectPort The redirect port number (non-SSL to SSL)
-		connector.setRedirectPort(443);
-		return connector;
-	}
-	@Bean
-	public EmbeddedServletContainerFactory servletContainer() {
-		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
-			@Override
-			protected void postProcessContext(Context context) {
-				//Due to CONFIDENTIAL and /*, this will cause Tomcat to redirect every request to HTTPS.
-				//You can configure multiple patterns and multiple constraints if you need more control over what is and is not redirected.
-
-				SecurityConstraint constraint = new SecurityConstraint();
-				constraint.setUserConstraint("CONFIDENTIAL");
-				SecurityCollection collection = new SecurityCollection();
-				collection.addPattern("/*");
-				constraint.addCollection(collection);
-				context.addConstraint(constraint);
-			}
-		};
-		tomcat.addAdditionalTomcatConnectors(httpConnector());
-		return tomcat;
-
-	}
+//	/**
+//	 * 会自动注册使用了@ServerEndpoint注解声明的Websocket endpoint
+//	 * 要注意，如果使用独立的servlet容器，
+//	 * 而不是直接使用springboot的内置容器，
+//	 * 就不要注入ServerEndpointExporter，因为它将由容器自己提供和管理。
+//	 */
+//	@Bean
+//	public ServerEndpointExporter serverEndpointExporter() {
+//		return new ServerEndpointExporter();
+//	}
+//
+//
+//	@Bean
+//	public Connector httpConnector() {
+//		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//
+//		//Set the scheme that will be assigned to requests received through this connector
+//		//@param scheme The new scheme
+//		connector.setScheme("http");
+//
+//		//Set the port number on which we listen for requests.
+//		// @param port The new port number
+//		connector.setPort(80);
+//
+//		//Set the secure connection flag that will be assigned to requests received through this connector.
+//		//@param secure The new secure connection flag
+//		//if connector.setSecure(true),the http use the http and https use the https;else if connector.setSecure(false),the http redirect to https;
+//		connector.setSecure(false);
+//
+//		//redirectPort The redirect port number (non-SSL to SSL)
+//		connector.setRedirectPort(443);
+//		return connector;
+//	}
+//	@Bean
+//	public EmbeddedServletContainerFactory servletContainer() {
+//		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+//			@Override
+//			protected void postProcessContext(Context context) {
+//				//Due to CONFIDENTIAL and /*, this will cause Tomcat to redirect every request to HTTPS.
+//				//You can configure multiple patterns and multiple constraints if you need more control over what is and is not redirected.
+//
+//				SecurityConstraint constraint = new SecurityConstraint();
+//				constraint.setUserConstraint("CONFIDENTIAL");
+//				SecurityCollection collection = new SecurityCollection();
+//				collection.addPattern("/*");
+//				constraint.addCollection(collection);
+//				context.addConstraint(constraint);
+//			}
+//		};
+//		tomcat.addAdditionalTomcatConnectors(httpConnector());
+//		return tomcat;
+//
+//	}
 }
--- src/main/java/com/melelee/melelee/controller/WebSocketController.java
+++ src/main/java/com/melelee/melelee/controller/WebSocketController.java
@@ -1,63 +1,63 @@
-package com.melelee.melelee.controller;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-
-import javax.websocket.*;
-import javax.websocket.server.PathParam;
-import javax.websocket.server.ServerEndpoint;
-import java.io.IOException;
-
-/**
- * webSocket
- *
- * @author mengll
- * @create 2018-11-22 10:28
- **/
-@Component
-@ServerEndpoint(value = "/mychat/{usernick}")
-@Slf4j
-public class WebSocketController {
-
-    /**
-     * 连接事件 加入注解
-     * @param session
-     */
-    @OnOpen
-    public void onOpen(@PathParam(value = "usernick") String userNick, Session session) {
-        String message = "有新游客[" + userNick + "]加入聊天室!";
-        log.info(message);
-        WebSocketUtil.addSession(userNick, session);
-        //此时可向所有的在线通知 某某某登录了聊天室
-        WebSocketUtil.sendMessageForAll(message);
-    }
-
-    @OnClose
-    public void onClose(@PathParam(value = "usernick") String userNick,Session session) {
-        String message = "游客[" + userNick + "]退出聊天室!";
-        log.info(message);
-        WebSocketUtil.remoteSession(userNick);
-        //此时可向所有的在线通知 某某某登录了聊天室
-        WebSocketUtil.sendMessageForAll(message);
-    }
-
-    @OnMessage
-    public void OnMessage(@PathParam(value = "usernick") String userNick, String message) {
-        //类似群发
-        String info = "游客[" + userNick + "]：" + message;
-        log.info(info);
-        WebSocketUtil.sendMessageForAll(message);
-    }
-
-    @OnError
-    public void onError(Session session, Throwable throwable) {
-        log.error("异常:", throwable);
-        try {
-            session.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        throwable.printStackTrace();
-    }
-
-}
+//package com.melelee.melelee.controller;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Component;
+//
+//import javax.websocket.*;
+//import javax.websocket.server.PathParam;
+//import javax.websocket.server.ServerEndpoint;
+//import java.io.IOException;
+//
+///**
+// * webSocket
+// *
+// * @author mengll
+// * @create 2018-11-22 10:28
+// **/
+//@Component
+//@ServerEndpoint(value = "/mychat/{usernick}")
+//@Slf4j
+//public class WebSocketController {
+//
+//    /**
+//     * 连接事件 加入注解
+//     * @param session
+//     */
+//    @OnOpen
+//    public void onOpen(@PathParam(value = "usernick") String userNick, Session session) {
+//        String message = "有新游客[" + userNick + "]加入聊天室!";
+//        log.info(message);
+//        WebSocketUtil.addSession(userNick, session);
+//        //此时可向所有的在线通知 某某某登录了聊天室
+//        WebSocketUtil.sendMessageForAll(message);
+//    }
+//
+//    @OnClose
+//    public void onClose(@PathParam(value = "usernick") String userNick,Session session) {
+//        String message = "游客[" + userNick + "]退出聊天室!";
+//        log.info(message);
+//        WebSocketUtil.remoteSession(userNick);
+//        //此时可向所有的在线通知 某某某登录了聊天室
+//        WebSocketUtil.sendMessageForAll(message);
+//    }
+//
+//    @OnMessage
+//    public void OnMessage(@PathParam(value = "usernick") String userNick, String message) {
+//        //类似群发
+//        String info = "游客[" + userNick + "]：" + message;
+//        log.info(info);
+//        WebSocketUtil.sendMessageForAll(message);
+//    }
+//
+//    @OnError
+//    public void onError(Session session, Throwable throwable) {
+//        log.error("异常:", throwable);
+//        try {
+//            session.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        throwable.printStackTrace();
+//    }
+//
+//}
