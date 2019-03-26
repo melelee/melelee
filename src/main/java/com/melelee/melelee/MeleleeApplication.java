3917bfbe9bae24b5ed9e252fc3a665f4c2ff9510
melelee <melelee@qq.com>
2019-03-26 15:41:28 +0800

    移除OAUTH2，集成测试demo

--- src/main/java/com/melelee/melelee/MeleleeApplication.java
+++ src/main/java/com/melelee/melelee/MeleleeApplication.java
@@ -2,16 +2,29 @@ package com.melelee.melelee;
 
 import com.didispace.swagger.EnableSwagger2Doc;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 
 /**
@@ -40,54 +53,26 @@ public class MeleleeApplication {
 		return new ServerEndpointExporter();
 	}
 
-	@Autowired
-	private RestTemplateBuilder restTemplateBuilder;
 
-	// 使用RestTemplateBuilder来实例化RestTemplate对象，spring默认已经注入了RestTemplateBuilder实例
-	@Bean
-	public RestTemplate restTemplate() {
-		return restTemplateBuilder.build();
+	@Bean(name = "sslRestTemplate")
+	public RestTemplate sslRestTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException, CertificateException, IOException, UnrecoverableKeyException {
+		KeyStore keyStore = KeyStore.getInstance("PKCS12");
+		File file = new File("config/iot.p12");
+		//如果配置文件不存在，从classpath加载配置
+		String passwd = "123123";
+		//读取证书
+		try (FileInputStream instream = new FileInputStream(file)) {
+			keyStore.load(instream, passwd.toCharArray());
+		}
+		SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(keyStore, new TrustSelfSignedStrategy()).build();
+
+		//不进行https主机域名认证，就是把https后面的域名与证书的域名匹配，如果需要可以实现自己的域名认证
+		HostnameVerifier hostnameVerifier = NoopHostnameVerifier.INSTANCE;
+		SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, hostnameVerifier);
+
+		CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+		requestFactory.setHttpClient(httpClient);
+		return new RestTemplate(requestFactory);
 	}
-
-//	@Bean
-//	public Connector httpConnector() {
-//		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//
-//		//Set the scheme that will be assigned to requests received through this connector
-//		//@param scheme The new scheme
-//		connector.setScheme("http");
-//
-//		//Set the port number on which we listen for requests.
-//		// @param port The new port number
-//		connector.setPort(80);
-//
-//		//Set the secure connection flag that will be assigned to requests received through this connector.
-//		//@param secure The new secure connection flag
-//		//if connector.setSecure(true),the http use the http and https use the https;else if connector.setSecure(false),the http redirect to https;
-//		connector.setSecure(false);
-//
-//		//redirectPort The redirect port number (non-SSL to SSL)
-//		connector.setRedirectPort(443);
-//		return connector;
-//	}
-//	@Bean
-//	public EmbeddedServletContainerFactory servletContainer() {
-//		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
-//			@Override
-//			protected void postProcessContext(Context context) {
-//				//Due to CONFIDENTIAL and /*, this will cause Tomcat to redirect every request to HTTPS.
-//				//You can configure multiple patterns and multiple constraints if you need more control over what is and is not redirected.
-//
-//				SecurityConstraint constraint = new SecurityConstraint();
-//				constraint.setUserConstraint("CONFIDENTIAL");
-//				SecurityCollection collection = new SecurityCollection();
-//				collection.addPattern("/*");
-//				constraint.addCollection(collection);
-//				context.addConstraint(constraint);
-//			}
-//		};
-//		tomcat.addAdditionalTomcatConnectors(httpConnector());
-//		return tomcat;
-//
-//	}
 }
--- src/main/java/com/melelee/melelee/yltest/YlTestService.java
+++ src/main/java/com/melelee/melelee/yltest/YlTestService.java
@@ -4,14 +4,18 @@ import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
 
 @Slf4j
 @Service
@@ -54,5 +58,61 @@ public class YlTestService {
 		} else {
 			return null;
 		}
+	}
+
+	@Value("${url.getbusinessid}")
+	private String urlgetbusinessid;
+
+	public String getBusinessId(String loginUserName) {
+
+		String token = "&access_token=";
+		String url = urlgetbusinessid + loginUserName + token + getToken();
+		JSONObject jsonObject = restTemplate.postForObject(url, null, JSONObject.class);
+		return jsonObject.getString("rspData");
+	}
+
+
+	@Value("${url.uploadfile}")
+	private String urluploadfile;
+	public JSONObject uploadFile(String loginUserName, String filePath) {
+		String token = "&access_token=" + getToken();
+		String businessId = "?businessId=" + getBusinessId(loginUserName);
+
+		MultiValueMap<String, Object> param = new LinkedMultiValueMap<>();
+		File file = new File(filePath);
+		if (file.isDirectory()){
+			for (File tmp : file.listFiles()) {
+				FileSystemResource resource = new FileSystemResource(tmp);
+				param.add("file", resource);
+			}
+		}else if (file.isFile()){
+			FileSystemResource resource = new FileSystemResource(file);
+			param.add("file", resource);
+		}
+		String postFileUrl = urluploadfile + businessId + token;
+		JSONObject jsonObject = restTemplate.postForObject(postFileUrl, param, JSONObject.class);
+
+		return jsonObject;
+
+	}
+
+	@Value("${dcm.path}")
+	private String dcmPath;
+	@Scheduled(fixedRate = 10000)
+	public void dcmPath() {
+		JSONObject jsonObject = this.uploadFile("yltest", dcmPath);
+
+		log.info("单个文件上传：{}", jsonObject);
+
+	}
+
+	@Value("${dcm.dir}")
+	private String dcmDir;
+	@Scheduled(fixedRate = 14000)
+	public void dcmDir() {
+		JSONObject jsonObject = this.uploadFile("yltest", dcmPath);
+
+		log.info("批量文件上传：{}", jsonObject);
+
 	}
 }
