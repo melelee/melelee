2e5956a503763e1e2c79fa044e45f52b603a9870
melelee <melelee@qq.com>
2019-06-25 20:26:48 +0800

    httpservletrequest api 使用

--- src/main/java/com/melelee/melelee/controller/UserController.java
+++ src/main/java/com/melelee/melelee/controller/UserController.java
@@ -5,11 +5,10 @@ import com.melelee.melelee.entity.User;
 import com.melelee.melelee.service.IUserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 
 /**
  * <p>
@@ -23,14 +22,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 @Slf4j
 public class UserController {
-	@Autowired
-	private IUserService iUserService;
+    @Autowired
+    private IUserService iUserService;
 
-	@PostMapping(value = "/save")
-	public String save(@RequestBody User user) {
-		boolean b = iUserService.save(user);
-		log.info("b {}", b);
-		System.out.println(1 / 0);
-		return "success";
-	}
+    @PostMapping(value = "/save")
+    public String save(HttpServletRequest httpServletRequest, @RequestBody User user) {
+        boolean b = iUserService.save(user);
+        log.info("b {}", b);
+        System.out.println(1 / 0);
+        return "success";
+    }
+    @GetMapping(value = "/test")
+    public String save(HttpServletRequest httpServletRequest) {
+        log.info("seseeion id {}", httpServletRequest.getSession().getId());
+        log.info("seseeion id {}", httpServletRequest.getSession(false).getId());
+        log.info("seseeion id {}", httpServletRequest.getSession(true).getId());
+        log.info("isRequestedSessionIdValid {}", httpServletRequest.isRequestedSessionIdValid());
+        log.info("getRealPath {}", httpServletRequest.getServletContext().getRealPath(File.separator));
+        log.info("getRealPath {}", httpServletRequest.getServletContext().getRealPath("|"));
+
+        return "success";
+    }
 }
