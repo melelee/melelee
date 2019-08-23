fd622f10498f0bfce91e3a2e9b50dfefc9674f2e
melelee <melelee@qq.com>
2019-08-23 13:26:20 +0800

    移除mybatispuls等依赖

--- src/main/java/com/melelee/melelee/controller/UserController.java
+++ src/main/java/com/melelee/melelee/controller/UserController.java
@@ -1,14 +1,9 @@
 package com.melelee.melelee.controller;
 
 
-import com.melelee.melelee.entity.User;
-import com.melelee.melelee.service.IUserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.File;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -22,25 +17,4 @@ import java.io.File;
 @RequestMapping("/user")
 @Slf4j
 public class UserController {
-    @Autowired
-    private IUserService iUserService;
-
-    @PostMapping(value = "/save")
-    public String save(HttpServletRequest httpServletRequest, @RequestBody User user) {
-        boolean b = iUserService.save(user);
-        log.info("b {}", b);
-        System.out.println(1 / 0);
-        return "success";
-    }
-    @GetMapping(value = "/test")
-    public String save(HttpServletRequest httpServletRequest) {
-        log.info("seseeion id {}", httpServletRequest.getSession().getId());
-        log.info("seseeion id {}", httpServletRequest.getSession(false).getId());
-        log.info("seseeion id {}", httpServletRequest.getSession(true).getId());
-        log.info("isRequestedSessionIdValid {}", httpServletRequest.isRequestedSessionIdValid());
-        log.info("getRealPath {}", httpServletRequest.getServletContext().getRealPath(File.separator));
-        log.info("getRealPath {}", httpServletRequest.getServletContext().getRealPath("|"));
-
-        return "success";
-    }
 }
