a0f3232b18878f29302ef21313e1411fc0b4909c
melelee <melelee@qq.com>
2019-08-26 18:43:30 +0800

    集成WxJava

--- src/main/java/com/melelee/melelee/controller/WechatController.java
+++ src/main/java/com/melelee/melelee/controller/WechatController.java
@@ -1,67 +1,108 @@
 package com.melelee.melelee.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.melelee.melelee.controller.bean.InWechatMsg;
-import com.melelee.melelee.controller.bean.OutWechatMsg;
-import com.melelee.melelee.exception.AesException;
-import com.melelee.melelee.utils.PasswordUtils;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONArray;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
-import springfox.documentation.spring.web.json.Json;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Date;
+
 
 @RestController
 @Slf4j
-@RequestMapping(value = "/wechat")
+@AllArgsConstructor
+@RequestMapping(value = "/wechat/{appid}")
 public class WechatController {
-    @GetMapping
-    public String token(@RequestParam String signature, @RequestParam String timestamp, @RequestParam String nonce, @RequestParam String echostr) throws NoSuchAlgorithmException, AesException {
-        log.info("wechat toke auth begin signature {},timestamp {},nonce {},echostr {}", signature, timestamp, nonce, echostr);
 
-        // 将sha1加密后的字符串可与signature对比，标识该请求来源于微信
-        if (PasswordUtils.verifyUrl(signature, timestamp, nonce)) {
-            return echostr;
-        } else {
-            return "go fuck yourself!";
+    private final WxMpService wxService;
+//    private final WxMpMessageRouter messageRouter;
+
+    @GetMapping(produces = "text/plain;charset=utf-8")
+    public String authGet(@PathVariable String appid,
+                          @RequestParam(name = "signature", required = false) String signature,
+                          @RequestParam(name = "timestamp", required = false) String timestamp,
+                          @RequestParam(name = "nonce", required = false) String nonce,
+                          @RequestParam(name = "echostr", required = false) String echostr) {
+
+        log.info("\n接收到来自微信服务器的认证消息：[{}, {}, {}, {}]", signature,
+                timestamp, nonce, echostr);
+        if (StringUtils.isAnyBlank(signature, timestamp, nonce, echostr)) {
+            throw new IllegalArgumentException("请求参数非法，请核实!");
         }
+
+        if (!this.wxService.switchover(appid)) {
+            throw new IllegalArgumentException(String.format("未找到对应appid=[%s]的配置，请核实！", appid));
+        }
+
+        if (wxService.checkSignature(timestamp, nonce, signature)) {
+            return echostr;
+        }
+
+        return "非法请求";
     }
 
     @PostMapping(produces = "application/xml; charset=UTF-8")
-    public OutWechatMsg message(@RequestParam String signature, @RequestParam String timestamp, @RequestParam String nonce,
-                                @RequestBody InWechatMsg inWechatMsg) throws AesException, JsonProcessingException {
+    public String post(@PathVariable String appid,
+                       @RequestBody String requestBody,
+                       @RequestParam("signature") String signature,
+                       @RequestParam("timestamp") String timestamp,
+                       @RequestParam("nonce") String nonce,
+                       @RequestParam("openid") String openid,
+                       @RequestParam(name = "encrypt_type", required = false) String encType,
+                       @RequestParam(name = "msg_signature", required = false) String msgSignature) {
+        log.info("\n接收微信请求：[openid=[{}], [signature=[{}], encType=[{}], msgSignature=[{}],"
+                        + " timestamp=[{}], nonce=[{}], requestBody=[\n{}\n] ",
+                openid, signature, encType, msgSignature, timestamp, nonce, requestBody);
 
-        log.info("wechat toke auth begin signature {},timestamp {},nonce {},message {}", signature, timestamp, nonce,
-                new ObjectMapper().writeValueAsString(inWechatMsg));
-
-
-        // 将sha1加密后的字符串可与signature对比，标识该请求来源于微信
-        if (PasswordUtils.verifyUrl(signature, timestamp, nonce)) {
-            //创建消息响应对象
-            OutWechatMsg outWechatMsg = new OutWechatMsg();
-            //把原来的发送方设置为接收方
-            outWechatMsg.setToUserName(inWechatMsg.getFromUserName());
-            //把原来的接收方设置为发送方
-            outWechatMsg.setFromUserName(inWechatMsg.getToUserName());
-            //获取接收的消息类型
-            String msgType = inWechatMsg.getMsgType();
-            //设置消息的响应类型
-            outWechatMsg.setMsgType(msgType);
-            //设置消息创建时间
-            outWechatMsg.setCreateTime(new Date().getTime());
-            //根据类型设置不同的消息数据
-            if ("text".equals(msgType)) {
-                outWechatMsg.setContent(inWechatMsg.getContent());
-            } else if ("image".equals(msgType)) {
-                outWechatMsg.setMediaId(new String[]{inWechatMsg.getMediaId()});
-            }
-            return outWechatMsg;
-        } else {
-            return null;
+        if (!this.wxService.switchover(appid)) {
+            throw new IllegalArgumentException(String.format("未找到对应appid=[%s]的配置，请核实！", appid));
         }
+
+        if (!wxService.checkSignature(timestamp, nonce, signature)) {
+            throw new IllegalArgumentException("非法请求，可能属于伪造的请求！");
+        }
+
+        String out = null;
+        if (encType == null) {
+            // 明文传输的消息
+            WxMpXmlMessage inMessage = WxMpXmlMessage.fromXml(requestBody);
+//            WxMpXmlOutMessage outMessage = this.route(inMessage);
+            WxMpXmlOutMessage outMessage = null;
+
+            if (outMessage == null) {
+                return "";
+            }
+
+            out = outMessage.toXml();
+        } else if ("aes".equalsIgnoreCase(encType)) {
+            // aes加密的消息
+            WxMpXmlMessage inMessage = WxMpXmlMessage.fromEncryptedXml(requestBody, wxService.getWxMpConfigStorage(),
+                    timestamp, nonce, msgSignature);
+            log.debug("\n消息解密后内容为：\n{} ", inMessage.toString());
+//            WxMpXmlOutMessage outMessage = this.route(inMessage);
+
+            WxMpXmlOutMessage outMessage = null;
+            if (outMessage == null) {
+                return "";
+            }
+
+            out = outMessage.toEncryptedXml(wxService.getWxMpConfigStorage());
+        }
+
+        log.debug("\n组装回复信息：{}", out);
+        return out;
     }
+
+//    private WxMpXmlOutMessage route(WxMpXmlMessage message) {
+//        try {
+//            return this.messageRouter.route(message);
+//        } catch (Exception e) {
+//            log.error("路由消息时出现异常！", e);
+//        }
+//
+//        return null;
+//    }
 }
